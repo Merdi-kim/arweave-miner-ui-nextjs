d505e3d2b6c1d24163da00bbfd9d5e9442126e7d
Merdi-kim <merdikimuha1@gmail.com>
2024-05-28 01:07:50 +0300

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,10 +16,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>Arweave Miner Metrics</title>
+        <link rel="icon" href="/assets/favicon.ico" sizes="any" />
         <meta
           name="description"
           content="Dashboard for displaying important Arweave miner metrics"
         />
+        
       </head>
       <body suppressHydrationWarning className={roboto.className}>
         <Navbar />
